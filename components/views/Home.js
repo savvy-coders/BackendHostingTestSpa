54e3bb19c7005a2809d193e7d3d286de2e4bdfdb
mjwebb1993 <mjwebb1993@yahoo.com>
2022-09-27 22:19:05 -0500

    Fixed Call to Action Button

--- components/views/Home.js
+++ components/views/Home.js
@@ -3,7 +3,7 @@ import html from "html-literal";
 export default state => html`
   <section id="jumbotron">
     <h2>SavvyCoders JavaScript Fullstack Bootcamp</h2>
-    <a href="index.html">"Call to Action" "Button"</a>
+    <a href="/">"Call to Action" "Button"</a>
   </section>
 
   <h3>
@@ -12,9 +12,3 @@ export default state => html`
     ${state.weather.feelsLike}F.
   </h3>
 `;
-
-{
-  /* <h3>
-The weather in ${state.weather.city} is ${state.Home.weather.description}. Temperature is ${store.weather.temp}F, and it feels like ${state.weather.feelsLike}F.
-</h3> */
-}
